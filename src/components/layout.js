605a4a9732c99b80b7579eefc61183fda00f4c13
Takuma Nagata <takuma.fn@gmail.com>
2020-04-17 17:31:30 +0900

    add maincontent.module.css.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+import styles from "../styles/maincontent.module.css"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -53,9 +54,8 @@ const Layout = ({ location, title, children }) => {
   }
   return (
     <div
+      className={styles.content}
       style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
         maxWidth: rhythm(24),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
